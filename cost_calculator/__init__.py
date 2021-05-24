0a61a6dd7e45b00f36fcb688f14ef55461fe5cd9
ORyomei <o.ryomei1020@gmail.com>
2021-05-24 22:46:20 +0900

    Add import sentence

--- cost_calculator/__init__.py
+++ cost_calculator/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.1.0"
 __title__ = "cost_calculator"
 
 from cost_calculator.cost_table import CostTable
-from cost_calculator.fca import Fca, FcaSheet
+from cost_calculator.fca import Fca, FcaSheet, pdfTofca
 from cost_calculator.bom import BomSheet
 from cost_calculator.costtable_to_fca import costTableToFca
 from cost_calculator.fca_to_bom import fcaToBom
